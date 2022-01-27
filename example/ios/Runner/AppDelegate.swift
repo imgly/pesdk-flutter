2fb784c6160278f8290826b15c098caf2e31c10a
Leon Dudlik <leon.dudlik@img.ly>
2022-01-27 08:35:12 +0100

    Add custom save icon for iOS

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import ImglyKit
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,15 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    PESDK.bundleImageBlock = { identifier in
+        if identifier == "imgly_icon_save" {
+            let data = try? Data(contentsOf: Bundle.main.url(forResource: "imgly_icon_approve_44pt", withExtension: "png")!)
+            return UIImage(data: data!)
+        }
+        return nil
+    }
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
